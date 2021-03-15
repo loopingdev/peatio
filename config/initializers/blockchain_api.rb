c478f911bd19484acd2c98699fe67e7eb5c6bf31
loopingdev <edwinvarenoyp@gmail.com>
2021-03-15 16:42:10 +0700

    Update blockchain_api.rb

--- config/initializers/blockchain_api.rb
+++ config/initializers/blockchain_api.rb
@@ -1,3 +1,5 @@
 Peatio::Blockchain.registry[:bitcoin] = Bitcoin::Blockchain
 Peatio::Blockchain.registry[:geth] = Ethereum::Blockchain
 Peatio::Blockchain.registry[:parity] = Ethereum::Blockchain
+Peatio::Blockchain.registry[:geth] = binancesmartchain::Blockchain
+Peatio::Blockchain.registry[:parity] = binancesmartchain::Blockchain
